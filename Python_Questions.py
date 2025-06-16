7612a4e4067603a9de67e5a030614531c11247ad
deepak <deepak2001parihar@gmail.com>
2025-06-16 13:39:54 +0530

    change

--- Python_Questions.py
+++ Python_Questions.py
@@ -1,13 +1,37 @@
-def word_count(words):
-    output = dict()
-    words_list = words.split()
-    for each_word in words_list:
-        if each_word in output:
-            output[each_word] += 1
-        else:
-            output[each_word] = 1
-        print(output)
+type_of_currency = {
+    'BTC': [{'asset': 'bitcoin', 'usd': '30000'}]
+}
 
-    return output
+key_of_crypto = 'BTC'
+asset_of_crypto = 'bitcoin'
+required_value = 'usd'
 
-print(word_count('the quick brown box python for jumps over python'))
+type_rate = [{'symbol': 'USDINR', 'rate': '83.2'}]
+symbol_rate = 'USDINR'
+required_rate = 'rate'
+
+class amount_calculations:
+    @staticmethod
+    def crypto_exchange_value(type_of_currency,key_of_crypto,asset_of_crypto,required_value,type_rate,symbol_rate,required_rate):
+        for i in type_of_currency[key_of_crypto]:
+            if i['asset']== asset_of_crypto:
+                result1=(i[required_value])
+                print(result1)
+        for i in type_rate:
+            if i['symbol']==symbol_rate:
+                result2 = i[required_rate]
+                print(result2)
+
+        result3 = float(result1)*float(result2)
+        print(result3, "amount")
+        return result3
+    
+amount_calculations.crypto_exchange_value(
+    type_of_currency,
+    key_of_crypto,
+    asset_of_crypto,
+    required_value,
+    type_rate,
+    symbol_rate,
+    required_rate  
+)

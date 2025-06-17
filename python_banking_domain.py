a77f33a2c676935dfefb6177677b530b37c0f7eb
deepak <deepak2001parihar@gmail.com>
2025-06-17 22:30:49 +0530

    changed

--- python_banking_domain.py
+++ python_banking_domain.py
@@ -1,4 +1,4 @@
-#hand_On Code- python getter & setter method to acces ,modify the private attributes in banking domain
+# #hand_On Code- python getter & setter method to access ,modify the private attributes in banking domain
 
 class hdfc_bank_account:
     def __init__(self, account_number, balance):
@@ -38,3 +38,10 @@ print(account.get_balance())
 
 account.withdraw_money(1500)
 account.set_balance(-200)
+
+# __ double underscore makes the balance attribute private. it cannot be accesses or modified directly outside of the class.
+# Gettter methos(get_balance):
+# this method retrives the value of the private __balance attribute.
+# it allows controlles access to the private attribute without exposing it directly
+# Setter method(set_balance): this method sets or modifies the value of __balance attribute.
+# it incli
